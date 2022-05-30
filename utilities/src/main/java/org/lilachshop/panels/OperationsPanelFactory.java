55300fae5086db6ca4b12eed96ee9861160c4ab7
Gil Kartaginer <73134488+gil-kart@users.noreply.github.com>
2022-05-30 10:54:51 +0300

    remove class flower and change to item (#18)
    Co-authored-by: Tal <talhachamsurkiss@gmail.com>

--- utilities/src/main/java/org/lilachshop/panels/OperationsPanelFactory.java
+++ utilities/src/main/java/org/lilachshop/panels/OperationsPanelFactory.java
@@ -1,32 +1,43 @@
 package org.lilachshop.panels;
 
-import org.jetbrains.annotations.Nullable;
-
 public class OperationsPanelFactory {
     private OperationsPanelFactory() {
     }
 
-
-    @Nullable
     public static Panel createPanel(int panel_num, Object controller) {    // for now int, later going to be enum!
         switch (panel_num) {
             case 1 -> {
                 return new ExamplePanel("localhost", 3000, controller);
             }
-            case 2 -> {
+            case 2 ->{
                 return new CustomerAnonymousPanel("localhost", 3000, controller);
             }
-            case 3 -> {
-                return new RegisteredCustomerPanel("localhost", 3000, controller);
+            case 3 ->{
+                return new StoreCustomerPanel("localhost", 3000, controller);
             }
-            case 4 -> {
-                return new CustomerServicePanel("localhost", 3000, controller);
+            case 4 ->{
+                return new ChainCustomerPanel("localhost", 3000, controller);
             }
-            case 5 -> {
+            case 5 ->{
+                return new AnnualCustomerPanel("localhost", 3000, controller);
+            }
+            case 6 ->{
                 return new EmployeeAnonymousPanel("localhost", 3000, controller);
             }
-            case 6 -> {
-                return new SysAdminPanel("localhost", 3000, controller);
+            case 7 ->{
+                return new GeneralEmployeePanel("localhost", 3000, controller);
+            }
+            case 8 ->{
+                return new StoreManagerPanel("localhost", 3000, controller);
+            }
+            case 9 ->{
+                return new ChainManagerPanel("localhost", 3000, controller);
+            }
+            case 10 ->{
+                return new CustomerServicePanel("localhost", 3000, controller);
+            }
+            case 11 ->{
+                return new ChainSystemPanel("localhost", 3000, controller);
             }
             default -> {
                 System.out.println("No panel found.");
