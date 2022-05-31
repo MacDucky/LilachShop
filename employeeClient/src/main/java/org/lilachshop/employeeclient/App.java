30a884b1c404c7106346e0c9ca42bee8ca8c3dbe
Dany Poroshan <33981442+MacDucky@users.noreply.github.com>
2022-05-31 14:53:04 +0300

    changed back to proper flow(main)

--- employeeClient/src/main/java/org/lilachshop/employeeclient/App.java
+++ employeeClient/src/main/java/org/lilachshop/employeeclient/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("IncomeReport"));
+        scene = new Scene(loadFXML("main"));
         stage.setScene(scene);
         stage.getScene().getWindow().addEventFilter(WindowEvent.WINDOW_CLOSE_REQUEST, this::onCloseWindowEvent);
         stage.show();
